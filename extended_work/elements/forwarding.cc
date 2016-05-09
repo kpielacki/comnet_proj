0971a833e31c601114bbfd969b0ae5d0255b6453
Anthony <yanga23@gmail.com>
2016-05-08 23:01:17 -0400

    fixed a lot of bugs

--- extended_work/elements/forwarding.cc
+++ extended_work/elements/forwarding.cc
@@ -9,11 +9,15 @@
 CLICK_DECLS
 
 Forwarding::Forwarding()  : r_table(0) {
-    num_dest = 0;
+
 }
 
 Forwarding::~Forwarding(){
 
+}
+
+int Forwarding::initialize(ErrorHandler *errh){
+    return 0;
 }
 
 int Forwarding::configure(Vector<String> &conf, ErrorHandler *errh){
@@ -21,7 +25,8 @@ int Forwarding::configure(Vector<String> &conf, ErrorHandler *errh){
     return 0;
 }
 
-void Forwarding::push(Packet *packet){
+void Forwarding::push(int port, Packet *packet){
+    num_dest = 0;
     pckt = packet;
     uint8_t pType = readPacket();
 
@@ -32,7 +37,7 @@ void Forwarding::push(Packet *packet){
     else if (pType==4){
         //Make array for possibleHops, set number of routes for each destination to 0
         for (int i = 0; i<num_dest; i++){
-            possibleHops[i][4] = 0;
+            possibleHops[i][3] = 0;
         }
 
         routing_entry* forwarding_table = r_table->get_all_entries();
@@ -40,18 +45,29 @@ void Forwarding::push(Packet *packet){
             for (int entry=0; entry <= r_table->get_entry_num(); entry++){
                 if (forwarding_table[entry].destination == dest[i]){
                     costs[i] = forwarding_table[entry].cost;
-                    possibleHops[i][possibleHops[i][4]] = forwarding_table[entry].next_hop;
-                    possibleHops[i][4]++;
+                    possibleHops[i][possibleHops[i][3]] = forwarding_table[entry].next_hop;
+                    possibleHops[i][3]++;
                 }
             }
 
+
             //Discard packet if routing entry not found
-            if (possibleHops[i][4]==0){
+            if (possibleHops[i][3]==0){
                 //packet->kill();
-                click_chatter("Forward Element - One or more destination not found in table. Unable to forward packet");
+                click_chatter("ERROR - Forward Element - addr: %u not found in table. Terminating" , dest[i]);
                 goto quit;
             }
         }
+
+        if ( k_val > num_dest){
+            click_chatter("ERROR - Value of k is greater than number of destinations. Terminating");
+            goto quit;
+        }
+
+        //Code to print out possibleHops
+        /* for(int i = 0; i <3; i++){
+            click_chatter("%u possibleHops D%u: %u %u %u %u", r_table->get_my_host(), (i+1), possibleHops[i][0], possibleHops[i][1], possibleHops[i][2], possibleHops[i][3]);
+        } */
 
         //Perform forwarding algorithm
         chooseKdest();
@@ -75,7 +91,9 @@ uint8_t Forwarding::readPacket(){
         d[0] = header->destination1;
         d[1] = header->destination2;
         d[2] = header->destination3;
-
+        click_chatter("%u found d1: %u", r_table->get_my_host(), header->destination1);
+        click_chatter("%u found d2: %u", r_table->get_my_host(), header->destination2);
+        click_chatter("%u found d3: %u", r_table->get_my_host(), header->destination3);
         for (int i = 0; i<3; i++){
             if ( d[i] == r_table->get_my_host() ){
                 // packet was meant for me
@@ -102,7 +120,7 @@ void Forwarding::chooseKdest(){
     if (k_val==3){
         findNextHop();
     }
-    if (k_val==2){
+    else if (k_val==2){
         if (num_dest>k_val){
             //Find index of highest cost destination
             int maxIndex = 0;
@@ -132,9 +150,9 @@ void Forwarding::chooseKdest(){
         }
         findNextHop();
     }
-    if (k_val==1){
+    else if (k_val==1){
         int leastIndex = 0;
-        for (int i = 1; i< num_dest; i++){
+        for (int i = 0; i< num_dest; i++){
             if (costs[i] < costs[leastIndex]){
                 leastIndex = i;
             }
@@ -143,15 +161,18 @@ void Forwarding::chooseKdest(){
         dest_index[leastIndex] = 1;
         forwardPacket(1, dest_index, possibleHops[leastIndex][0]);
     }
+    else{
+        click_chatter("Forward Element - k is invalid!");
+    }
 }
 
 void Forwarding::findNextHop(){
     bool done = false;
     //Finding common nextHops for 3-pair, k=3 only
     if (k_val == 3){
-        for (int i1 = 0; i1<possibleHops[i1][4]; i1++){
-            for (int i2 = 0; i2<possibleHops[i2][4]; i2++){
-                for (int i3 = 0; i3<possibleHops[i3][4]; i3++){
+        for (int i1 = 0; i1<possibleHops[0][3]; i1++){
+            for (int i2 = 0; i2<possibleHops[1][3]; i2++){
+                for (int i3 = 0; i3<possibleHops[2][3]; i3++){
                     if (possibleHops[0][i1] == possibleHops[1][i2] && possibleHops[1][i2] == possibleHops[2][i3]){
                         int dest_index[] = {1,1,1};
                         forwardPacket(3, dest_index, possibleHops[0][i1]);
@@ -167,18 +188,18 @@ void Forwarding::findNextHop(){
         for (int i1 = 0; i1 < k_val-1; i1++){
             for (int j1 = i1+1; j1<k_val; j1++){
             //Only check for a common nextHop if the current choices are different
-                for (int i2 = 0; i2<possibleHops[i1][4]; i2++){
-                    for (int j2 = 0; j2<possibleHops[j1][4]; j2++){
+                for (int i2 = 0; i2<possibleHops[i1][3]; i2++){
+                    for (int j2 = 0; j2<possibleHops[j1][3]; j2++){
                         if (possibleHops[i1][i2] == possibleHops[j1][j2]){
                             //Put multicasted destinations together in new array
                             int dest_index[] = {1,1,1};
-                            dest_index[3-i1-i2] = 0;
+                            dest_index[3-i1-j1] = 0;
                             forwardPacket(2, dest_index, possibleHops[i1][i2]);
                             //Still need to send last packet if k = 3
                             if (k_val==3){
                                 int dest_index[] = {0,0,0};
-                                dest_index[3-i1-i2] = 1;
-                                forwardPacket(1, dest_index, possibleHops[3-i1-i2][0]);
+                                dest_index[3-i1-j1] = 1;
+                                forwardPacket(1, dest_index, possibleHops[3-i1-j1][0]);
                             }
                             done = true;
                         }
@@ -230,8 +251,7 @@ void Forwarding::forwardPacket(uint8_t new_k, int dest_index[3], uint16_t nextHo
     packet->set_anno_u16(anno, nextHop);
 
     output(0).push(packet);
-
-    click_chatter("Forwarding packet with k: %u", new_k);
+    click_chatter("%u forwarding packet with k: %u to addr: %u", r_table->get_my_host(), new_k, nextHop);
 }
 
 CLICK_ENDDECLS
